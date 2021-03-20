e7cb6eab8303be64fc1517dcdaefbb9b82a8b7f7
Jared Faris <jfaris@gmail.com>
2021-03-20 09:39:32 -0400

    Site updated: 2021-03-20 09:39:32

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-var CACHE = 'network-or-cache-1616247241066';
+var CACHE = 'network-or-cache-1616247570725';
 
 // On install, cache some resource.
 self.addEventListener('install', function(evt) {

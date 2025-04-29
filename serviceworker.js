42c5820d4177e3c3d92f789936628b677470b5d9
jaredfaris <jaredfaris@users.noreply.github.com>
2025-04-29 01:05:52 +0000

    deploy: db05babe0df9c5434a7643e85b7b35f7c997d6f9

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-var CACHE = 'network-or-cache-1745857009872';
+var CACHE = 'network-or-cache-1745888748839';
 
 // On install, cache some resource.
 self.addEventListener('install', function(evt) {
@@ -25,25 +25,25 @@ self.addEventListener('fetch', function(evt) {
     return caches.open(CACHE).then(function (cache) {
         return cache.addAll([
             "/images/speakers/2022/Readme.html",
+"/manifest.json",
 "/",
 "/index.html",
-"/manifest.json",
-"/404.html",
 "/covid/",
 "/covid/index.html",
+"/404.html",
 "/recordings/",
 "/recordings/index.html",
 "/registration/",
 "/registration/index.html",
 "/schedule/",
 "/schedule/index.html",
-"/speakers/2018.html",
 "/sessions/",
 "/sessions/index.html",
+"/speakers/2018.html",
 "/speakers/2019.html",
 "/speakers/2020.html",
-"/speakers/2022.html",
 "/speakers/2021.html",
+"/speakers/2022.html",
 "/speakers/2023.html",
 "/speakers/2024.html",
 "/speakers/",
@@ -62,10 +62,10 @@ self.addEventListener('fetch', function(evt) {
 "/info/antiharassmentpolicy/index.html",
 "/info/codeofconduct/",
 "/info/codeofconduct/index.html",
-"/info/commitmenttodiversity/",
-"/info/commitmenttodiversity/index.html",
 "/info/contact/",
 "/info/contact/index.html",
+"/info/commitmenttodiversity/",
+"/info/commitmenttodiversity/index.html",
 "/info/history/",
 "/info/history/index.html",
 "/info/press/2024-02-10.html",

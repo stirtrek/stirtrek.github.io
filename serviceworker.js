0b71a5898da4237273f2aeb982c72ffbdb066978
jaredfaris <jaredfaris@users.noreply.github.com>
2025-05-03 08:16:42 +0000

    deploy: 99b90a55e62a18d53708966019aa625c59d82b74

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-var CACHE = 'network-or-cache-1746234289286';
+var CACHE = 'network-or-cache-1746260199016';
 
 // On install, cache some resource.
 self.addEventListener('install', function(evt) {
@@ -25,10 +25,10 @@ self.addEventListener('fetch', function(evt) {
     return caches.open(CACHE).then(function (cache) {
         return cache.addAll([
             "/images/speakers/2022/Readme.html",
+"/manifest.json",
 "/",
 "/index.html",
 "/404.html",
-"/manifest.json",
 "/covid/",
 "/covid/index.html",
 "/recordings/",
@@ -40,18 +40,18 @@ self.addEventListener('fetch', function(evt) {
 "/sessions/",
 "/sessions/index.html",
 "/speakers/2018.html",
-"/speakers/2020.html",
 "/speakers/2019.html",
+"/speakers/2020.html",
 "/speakers/2021.html",
-"/speakers/2023.html",
 "/speakers/2022.html",
+"/speakers/2023.html",
 "/speakers/2024.html",
 "/speakers/",
 "/speakers/index.html",
 "/speakers/index_cfpopenversion.html",
-"/speakers/index_noncfpversion.html",
 "/sponsors/",
 "/sponsors/index.html",
+"/speakers/index_noncfpversion.html",
 "/store/",
 "/store/index.html",
 "/virtual/",
@@ -60,10 +60,10 @@ self.addEventListener('fetch', function(evt) {
 "/workshops/index.html",
 "/info/antiharassmentpolicy/",
 "/info/antiharassmentpolicy/index.html",
-"/info/codeofconduct/",
-"/info/codeofconduct/index.html",
 "/info/commitmenttodiversity/",
 "/info/commitmenttodiversity/index.html",
+"/info/codeofconduct/",
+"/info/codeofconduct/index.html",
 "/info/contact/",
 "/info/contact/index.html",
 "/info/history/",
@@ -78,8 +78,8 @@ self.addEventListener('fetch', function(evt) {
 "/styles/fontawesome-6/css/brands.min.css",
 "/styles/fontawesome-6/css/regular.css",
 "/styles/fontawesome-6/css/regular.min.css",
-"/styles/fontawesome-6/css/solid.css",
 "/styles/fontawesome-6/css/solid.min.css",
+"/styles/fontawesome-6/css/solid.css",
 "/styles/fontawesome-6/css/svg-with-js.css",
 "/styles/fontawesome-6/css/svg-with-js.min.css",
 "/styles/fontawesome-6/css/v4-font-face.css",
@@ -92,8 +92,8 @@ self.addEventListener('fetch', function(evt) {
 "/styles/fontawesome-6/css/brands.css",
 "/styles/fontawesome-6/css/v4-shims.min.css",
 "/scripts/bootstrap.min.js",
-"/styles/fontawesome-all.min.css",
 "/styles/fontawesome.min.css",
+"/styles/fontawesome-all.min.css",
 "/styles/fontawesome-6/css/v4-shims.css",
 "/styles/fontawesome-6/css/fontawesome.min.css",
 "/scripts/bootstrap.js",

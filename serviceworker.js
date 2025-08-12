e64973c6ee3be5eddd1e1ae87c05c291775f3117
jaredfaris <jaredfaris@users.noreply.github.com>
2025-08-12 08:20:20 +0000

    deploy: 6fdfa78129e07d610787e9bab86dd26ce5df2221

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-var CACHE = 'network-or-cache-1754960984537';
+var CACHE = 'network-or-cache-1754986816884';
 
 // On install, cache some resource.
 self.addEventListener('install', function(evt) {
@@ -39,18 +39,18 @@ self.addEventListener('fetch', function(evt) {
 "/schedule/index.html",
 "/sessions/",
 "/sessions/index.html",
-"/speakers/2019.html",
 "/speakers/2018.html",
-"/speakers/2020.html",
+"/speakers/2019.html",
 "/speakers/2021.html",
+"/speakers/2020.html",
 "/speakers/2022.html",
+"/speakers/2023.html",
 "/speakers/2024.html",
 "/speakers/2025.html",
-"/speakers/2023.html",
 "/speakers/index_cfpopenversion.html",
+"/speakers/index_noncfpversion.html",
 "/speakers/",
 "/speakers/index.html",
-"/speakers/index_noncfpversion.html",
 "/sponsors/",
 "/sponsors/index.html",
 "/store/",
@@ -78,15 +78,15 @@ self.addEventListener('fetch', function(evt) {
 "/info/travel/index.html",
 "/styles/fontawesome-6/css/brands.min.css",
 "/styles/fontawesome-6/css/regular.css",
-"/styles/fontawesome-6/css/regular.min.css",
 "/styles/fontawesome-6/css/solid.css",
-"/styles/fontawesome-6/css/svg-with-js.min.css",
+"/styles/fontawesome-6/css/regular.min.css",
 "/styles/fontawesome-6/css/solid.min.css",
 "/styles/fontawesome-6/css/svg-with-js.css",
+"/styles/fontawesome-6/css/svg-with-js.min.css",
 "/styles/fontawesome-6/css/v4-font-face.css",
 "/styles/fontawesome-6/css/v4-font-face.min.css",
-"/styles/fontawesome-6/css/v5-font-face.css",
 "/styles/fontawesome-6/css/v5-font-face.min.css",
+"/styles/fontawesome-6/css/v5-font-face.css",
 "/styles/bootstrap-button-social.min.css",
 "/styles/fontawesome-4.min.css",
 "/styles/site.css",

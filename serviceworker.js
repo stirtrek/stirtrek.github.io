9b6713661de06f9f4f1d1eb274655d0a24756c54
jaredfaris <jaredfaris@users.noreply.github.com>
2025-10-29 01:09:25 +0000

    deploy: c8462fc77a789b798026d56e5188ba65560c8c5c

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-var CACHE = 'network-or-cache-1761668341537';
+var CACHE = 'network-or-cache-1761700161650';
 
 // On install, cache some resource.
 self.addEventListener('install', function(evt) {
@@ -42,8 +42,8 @@ self.addEventListener('fetch', function(evt) {
 "/speakers/2018.html",
 "/speakers/2019.html",
 "/speakers/2020.html",
-"/speakers/2021.html",
 "/speakers/2022.html",
+"/speakers/2021.html",
 "/speakers/2023.html",
 "/speakers/2024.html",
 "/speakers/2025.html",
@@ -70,10 +70,10 @@ self.addEventListener('fetch', function(evt) {
 "/info/history/",
 "/info/history/index.html",
 "/info/press/2024-02-10.html",
-"/info/privacy/",
-"/info/privacy/index.html",
 "/info/press/",
 "/info/press/index.html",
+"/info/privacy/",
+"/info/privacy/index.html",
 "/info/travel/",
 "/info/travel/index.html",
 "/styles/fontawesome-6/css/brands.min.css",

780b34992842f731d8da190ef03adb7e136b8408
jaredfaris <jaredfaris@users.noreply.github.com>
2026-01-20 01:14:28 +0000

    deploy: 2ac15b7e15d529a98e3bcd1254af8260a332a4a0

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 
-var CACHE = 'network-or-cache-1768839686561';
+var CACHE = 'network-or-cache-1768871664533';
 
 // On install, cache some resource.
 self.addEventListener('install', function(evt) {
@@ -27,8 +27,8 @@ self.addEventListener('fetch', function(evt) {
             "/images/speakers/2022/Readme.html",
 "/",
 "/index.html",
-"/manifest.json",
 "/404.html",
+"/manifest.json",
 "/covid/",
 "/covid/index.html",
 "/recordings/",
@@ -44,11 +44,11 @@ self.addEventListener('fetch', function(evt) {
 "/speakers/2019.html",
 "/speakers/2021.html",
 "/speakers/2022.html",
-"/speakers/2023.html",
 "/speakers/2024.html",
+"/speakers/2023.html",
+"/speakers/2025.html",
 "/speakers/",
 "/speakers/index.html",
-"/speakers/2025.html",
 "/speakers/index_cfpopenversion.html",
 "/speakers/index_noncfpversion.html",
 "/sponsors/",
@@ -59,10 +59,10 @@ self.addEventListener('fetch', function(evt) {
 "/virtual/index.html",
 "/workshops/",
 "/workshops/index.html",
-"/info/codeofconduct/",
-"/info/codeofconduct/index.html",
 "/info/antiharassmentpolicy/",
 "/info/antiharassmentpolicy/index.html",
+"/info/codeofconduct/",
+"/info/codeofconduct/index.html",
 "/info/commitmenttodiversity/",
 "/info/commitmenttodiversity/index.html",
 "/info/contact/",
@@ -82,11 +82,11 @@ self.addEventListener('fetch', function(evt) {
 "/styles/fontawesome-6/css/solid.css",
 "/styles/fontawesome-6/css/solid.min.css",
 "/styles/fontawesome-6/css/svg-with-js.css",
+"/styles/fontawesome-6/css/svg-with-js.min.css",
 "/styles/fontawesome-6/css/v4-font-face.css",
 "/styles/fontawesome-6/css/v4-font-face.min.css",
-"/styles/fontawesome-6/css/svg-with-js.min.css",
-"/styles/fontawesome-6/css/v5-font-face.css",
 "/styles/fontawesome-6/css/v5-font-face.min.css",
+"/styles/fontawesome-6/css/v5-font-face.css",
 "/styles/bootstrap-button-social.min.css",
 "/styles/fontawesome-4.min.css",
 "/styles/site.css",
